0d56848eb0e90aa0f198a6f30069e36ebcc2eabb
jesusFreak1 <32850733+kkellogg378@users.noreply.github.com>
2024-02-12 18:17:58 -0500

    added classes for different pieces

--- chess.py
+++ chess.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 
 # Define names
-pawn = "Pawn"
-rook = "Rook"
-knight = "Knight"
-bishop = "Bishop"
-queen = "Queen"
-king = "King"
+#pawn = "Pawn"
+#rook = "Rook"
+#knight = "Knight"
+#bishop = "Bishop"
+#queen = "Queen"
+#king = "King"
 
 white = "White"
 black = "Black"
@@ -21,11 +21,45 @@ window['padx'] = 10
 window['pady'] = 10
 
 class chessPiece:
-    def __init___(self, pieceType, team):
+    def __init___(self, pieceType, team, i, j):
         self.pieceType = pieceType
         self.team = team
+        self.i = i 
+        self.j = j 
     
     def __str__(self):
         return f"{team} {self.pieceType}"
-        
+    
+
+class pawn(chessPiece):
+    def __init__(self, team, i, j):
+        chessPiece.__init__(self, "Pawn", team, i, j)
+    
+
+class rook(chessPiece):
+    def __init__(self, team, i, j):
+        chessPiece.__init__(self, "Rook", team, i, j)
+    
+
+class knight(chessPiece):
+    def __init__(self, team, i, j):
+        chessPiece.__init__(self, "Knight", team, i, j)
+    
+
+class bishop(chessPiece):
+    def __init__(self, team, i, j):
+        chessPiece.__init__(self, "Bishop", team, i, j)
+    
+
+class queen(chessPiece):
+    def __init__(self, team, i, j):
+        chessPiece.__init__(self, "Queen", team, i, j)
+    
+
+class king(chessPiece):
+    def __init__(self, team, i, j):
+        chessPiece.__init__(self, "King", team, i, j)
+    
+
+
     
